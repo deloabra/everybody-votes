62092bab6bdcb61c36aa9e96f4f598a34fb83350
Brayden DeLoach <brayden@deloach.us>
2020-05-05 14:25:58 -0500

    Created seed sql file that creates basic tables.

---
+++ seed.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS everybody_votes;
+CREATE DATABASE everybody_votes;
+
+USE everybody_votes;
+
+CREATE TABLE question(
+	id INT NOT NULL AUTO_INCREMENT,
+    question VARCHAR(500),
+    CreatedAt DATETIME DEFAULT current_timestamp,
+    ExpiresAt DATETIME DEFAULT current_timestamp,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE vote(
+	id INT NOT NULL AUTO_INCREMENT,
+    ip varchar(20),
+    questionid INT,
+    CreatedAt DATETIME DEFAULT current_timestamp,
+    PRIMARY KEY(id)
+);
