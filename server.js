2375d935a7feccfbb67e39c53a65170cdb0a2d5f
Brayden DeLoach <brayden@deloach.us>
2020-07-21 17:10:13 -0500

    made server file create the server and added necessary dependencies

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+}
+//define API routes here
+
+
+//define send main page
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+});
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import './homepage.css';
+import React, { useState } from 'react';
+import './Homepage.css';
+import { Pie } from 'react-chartjs-2';
 
-function homepage() {
+function Homepage() {
+
+  //state for chart
+  const [pieState, setPieState] = useState({
+    labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: [
+        '#B21F00',
+        '#C9DE00',
+        '#2FDE00',
+        '#00A6B4',
+        '#6800B4'
+      ],
+      hoverBackgroundColor: [
+      '#501800',
+      '#4B5000',
+      '#175000',
+      '#003350',
+      '#35014F'
+      ],
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -30,11 +59,27 @@ function homepage() {
         </div>
       </nav>
 
-
+      {/* chart test */}
+      <div>
+      <Pie
+          data={pieState}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
 
 
     </div>
   );
 }
 
-export default homepage;
+export default Homepage;
