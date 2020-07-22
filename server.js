b2797baf111f187d6d06d0152ec41eb09e7a021b
Brayden DeLoach <brayden@deloach.us>
2020-07-22 14:23:43 -0500

    added a test api route and made sure it worked.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 //middleware
@@ -8,14 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //static assets
+//maybe need to change to /client/build
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
 }
-else{
-    app.use(express.static("client"));
-}
 //define API routes here
 
+//test api route
+app.get('/test', (req, res) => {
+    res.send("test");
+});
 
 //define send main page
 app.get('*', (req, res) => {
