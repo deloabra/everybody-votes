da1ae19bf468b9608d307075d85eb7bb880f1494
Brayden DeLoach <brayden@deloach.us>
2020-09-09 15:44:41 -0500

    Added default file to send in production.

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 //server doesn't give react page if not in production
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("/client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 //require routes
