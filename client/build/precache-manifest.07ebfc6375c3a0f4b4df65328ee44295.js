7d47edf0ef44153ee74f862c781b45475b01a21e
Brayden DeLoach <brayden@deloach.us>
2020-09-09 17:04:49 -0500

    pre built client and made heroku postbuild blank so heroku doesn't have to build.

---
+++ client/build/precache-manifest.07ebfc6375c3a0f4b4df65328ee44295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaaf3311c5761839ef4d584bd1eab290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bdee74f5380f102932e",
+    "url": "/static/css/main.da726dbc.chunk.css"
+  },
+  {
+    "revision": "187cfd6d428e6f135021",
+    "url": "/static/js/2.222c03d0.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.222c03d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdee74f5380f102932e",
+    "url": "/static/js/main.5e4f1e47.chunk.js"
+  },
+  {
+    "revision": "b4ed5a8bbed25d19f209",
+    "url": "/static/js/runtime-main.49f2ddeb.js"
+  }
+]);
