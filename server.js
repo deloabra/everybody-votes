7bdf7a2bc207bb2f7d450b118ecd85c81b8d2c0c
Brayden DeLoach <brayden@deloach.us>
2020-09-09 16:27:44 -0500

    fixed static assets by removing preceding / in client/build

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(express.json());
 //static assets
 //server doesn't give react page if not in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("/client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+    app.use(express.static("client/build"));
 }
 
 //require routes
